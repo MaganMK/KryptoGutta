d3e95b49078dacaf4117514e550d937f847707aa
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2018-04-09 14:33:46 +0200

    fungerende? getPrice for dato

--- backend/HTTPhandler.js
+++ backend/HTTPhandler.js
@@ -1,48 +1,29 @@
-var response = "";
-
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    var resp = "";
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-            response = xmlHttp.responseText;
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.send(null);
-    return resp;
-}
-
-function callback(text) {
-	//Håndterer tekst fra api-kallet
-}
-
 // Returnerer prisen for en enhet av fromCurrency i toCurrency på angitt dato
 // Funker kanskje bare med usd
-function getPrice(date, fromCurrency, toCurrecy)
+function getPrice(date, fromCurrency, toCurrency)
 {
     //Usikker på om getTime gir riktig timestamp
     let url = "https://min-api.cryptocompare.com/data/pricehistorical?fsym="
     + fromCurrency + "&tsyms=" + toCurrency + "&ts=" + date.getTime();
-    let result = httpGetAsync(url);
-    let obj = JSON.parse(result);
-    return obj[currency][toCurrency];
-}
-
-function getPrice(url)
-{
-fetch(url)
-  .then(function(response) {
-    response.text().then(function(text) {
-      storedText = text;
-      done();
+    var obj;
+    jQuery.when(
+        jQuery.getJSON(url)
+    ).done( function(json) {
+        callback(json);
     });
-  });
-}
-function handleText() {
-  document.getElementById('i').textContent =
-    "Here's what I got! \n" + storedText;
+    obj = loadJSON();
+    return obj[fromCurrency][toCurrency];
 }
 
-getUrl(url);
+function callback(text) {
+    saveJSON(text);
+}
+
+function saveJSON(obj)
+{
+    sessionStorage.setItem("price", JSON.stringify(obj));
+}
+
+function loadJSON() {
+    return JSON.parse(sessionStorage.getItem("price"));
+}
